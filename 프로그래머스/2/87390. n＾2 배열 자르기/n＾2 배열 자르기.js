0e61e36c294e2b4553b32768563022fee437d735
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2025-01-20 15:10:34 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.24 ms, Memory: 50.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,12 +1,10 @@
 function solution(n, left, right) {
     const result = [];
-    
-    // left부터 right까지의 값을 계산하여 result 배열에 채웁니다.
+
     for (let i = left; i <= right; i++) {
-        const row = Math.floor(i / n);
-        const col = i % n;
-        result.push(Math.max(row, col) + 1);
+        // 각 인덱스에서의 값은 Math.max(행, 열) + 1로 계산
+        result.push(Math.max(Math.floor(i / n), i % n) + 1);
     }
-    
+
     return result;
 }
