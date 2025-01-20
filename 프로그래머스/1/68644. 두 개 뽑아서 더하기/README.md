de33ad104d19d198018fd2487360b3f9a32bf6b2
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2025-01-20 14:54:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.88 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.59 ms
+메모리: 33.5 MB, 시간: 0.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 03:33:35
+2025년 01월 20일 14:54:45
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,9 @@
 function solution(numbers) {
-    const temp = []
-
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            temp.push(numbers[i] + numbers[j])
+    const ret = [];
+    for (let i = 0 ; i< numbers.length; i++){
+        for (let j =0; j< i; j++){
+            ret.push(numbers[i] + numbers[j]);
         }
     }
-
-    const answer = [...new Set(temp)]
-
-    return answer.sort((a, b) => a - b)
+    return [...new Set(ret)].sort((a,b)=> a - b);
 }
